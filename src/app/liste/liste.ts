3ba65676a46acd59afae5f7c1a25f61d8aa30570
Quentin <deveaud.quentin@gmail.com>
2025-09-17 16:29:28 +0200

    essai

--- src/app/liste/liste.ts
+++ src/app/liste/liste.ts
@@ -9,10 +9,10 @@ import { Component } from '@angular/core';
 export class Liste {
 
   public listFilms = [
-    {id:1, name: 'nom1'},
-    {id:2, name: 'nom2'},
-    {id:3, name: 'nom3'},
-    {id:4, name: 'nom4'},
-    {id:5, name: 'nom5'}
+    {id:1, name: 'nom1', affiche:'poster_aftersun.jpg'},
+    {id:2, name: 'nom2', affiche:'poster_grand-budapest-hotel.jpg'},
+    {id:3, name: 'nom3', affiche:'poster_julie-en-12-chapitres.jpg'},
+    {id:4, name: 'nom4', affiche:'poster_lost-in-translation.jpg'},
+    {id:5, name: 'nom5', affiche:'poster_past-lives.jpg'}
   ];
 }
