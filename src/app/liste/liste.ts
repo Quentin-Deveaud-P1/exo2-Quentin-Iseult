971ac9bc6b3f9ff476d96d09d862c5a89c2906f9
Quentin <deveaud.quentin@gmail.com>
2025-09-22 11:43:33 +0200

    Correctif V1

--- src/app/liste/liste.ts
+++ src/app/liste/liste.ts
@@ -9,10 +9,10 @@ import { Component } from '@angular/core';
 export class Liste {
   public listFilms = [
     {id:1, name: 'Aftersun', affiche:'poster_aftersun.jpg'},
-    {id:2, name: 'Grand Budapest Hotel', affiche:'poster_grand-budapest-hotel.jpg'},
-    {id:3, name: 'Julie en 12 chapitres', affiche:'poster_julie-en-12-chapitres.jpg'},
-    {id:4, name: 'Lost in translation', affiche:'poster_lost-in-translation.jpg'},
-    {id:5, name: 'Past lives', affiche:'poster_past-lives.jpg'}
+    {id:2, name: 'The Grand Budapest Hotel', affiche:'poster_grand-budapest-hotel.jpg'},
+    {id:3, name: 'Julie en 12 Chapitres', affiche:'poster_julie-en-12-chapitres.jpg'},
+    {id:4, name: 'Lost in Translation', affiche:'poster_lost-in-translation.jpg'},
+    {id:5, name: 'Past Lives', affiche:'poster_past-lives.jpg'}
   ];
 
   selectedFilm: any = null;
